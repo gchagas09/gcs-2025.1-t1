1a89c70b0bcd75b7749d765632c4ca5b211ef01c
Joao <joao.rangel@edu.pucrs.br>
2025-04-16 12:06:40 -0300

    Classe departamento, teste v1

---
+++ src/Departamento.java
@@ -0,0 +1,9 @@
+public class Departamento {
+    String nome;
+    double limite;
+
+    public Departamento(String nome, double limite) {
+        this.nome = nome;
+        this.limite = limite;
+    }
+}
