ab7a8178fbfa3bc698dc9845f3c5063a417682c4
gchagas09 <gchagas090299@outlook.com.br>
2025-04-25 18:14:43 -0300

    feat: enums do sistema
    Feita adição da enum StatusPedido e modificada a classe Departamento, para melhor representar o planejamento do sistema

--- enums/Departamento.java
+++ enums/Departamento.java
@@ -1,10 +1,24 @@
 package enums;
-public class Departamento {
-    String nome;
-    double limite;
+public enum Departamento {
+    RECURSOS_HUMANOS(1000), 
+    FINANCEIRO(1000),
+    MARKETING(1000),
+    VENDAS(1000),
+    DIRETORIA(1000),
+    TI(1000),
+    OPERACOES(1000);
+    
+    private double limitePedido;
 
-    public Departamento(String nome, double limite) {
-        this.nome = nome;
-        this.limite = limite;
+    private Departamento(double limitePedido){
+        this.limitePedido = limitePedido;
     }
+
+    public double getLimite(){
+        return this.limitePedido;
+    }
+    public void setLimite(double limitePedido){
+        this.limitePedido = limitePedido;
+    }
+    
 }
