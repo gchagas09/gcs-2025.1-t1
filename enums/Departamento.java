d5074f3397fa24965f56935a00e99acc4660c078
gchagas09 <gchagas090299@outlook.com.br>
2025-04-25 17:56:34 -0300

    fix: modificada localizaçao de arquivo
    Arquivo movido para melhor organização do projeto

---
+++ enums/Departamento.java
@@ -0,0 +1,10 @@
+package enums;
+public class Departamento {
+    String nome;
+    double limite;
+
+    public Departamento(String nome, double limite) {
+        this.nome = nome;
+        this.limite = limite;
+    }
+}
